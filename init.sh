57840c193283af7c974880eb520e7f677c2e07be
SoyaOhnishi <soya.ohnishi@gmail.com>
2022-04-28 19:13:18 +0900

    Fix some settings and Add Cargo.toml
    To work with rust-analyzer well, delete some rust analyzer settings
    Also, add Cargo.toml to capture where are each projects.

--- init.sh
+++ init.sh
@@ -3,4 +3,4 @@
 wget https://raw.githubusercontent.com/ros2-rust/ros2_rust/master/ros2_rust_foxy.repos -P /tmp
 vcs import src < /tmp/ros2_rust_foxy.repos
 
-colcon build
+colcon build --packages-ignore rosidl_runtime_rs rclrs
