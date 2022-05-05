9c1097d1e59bb444861329435d517cf844aac30d
SoyaOhnishi <soya.ohnishi@gmail.com>
2022-05-05 02:11:50 +0000

    Fix project creation flow

--- init.sh
+++ init.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+git submodule add $ROS2_RUST_REPO src/ros2_rust
+git submodule init
+git submodule update
+
 wget https://raw.githubusercontent.com/ros2-rust/ros2_rust/master/ros2_rust_foxy.repos -P /tmp
 vcs import src < /tmp/ros2_rust_foxy.repos
 
